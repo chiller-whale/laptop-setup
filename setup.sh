3546e188303d8bfa0215ce5c72bdb580cc3425e4
Tyler Freeman <tylerfrmn5@gmail.com>
2021-04-11 18:28:30 -0400

    add dock config to git

--- setup.sh
+++ setup.sh
@@ -8,15 +8,20 @@
 # brew bundle;
 
 # Organize Dock
-defaults write com.apple.dock persistent-apps -array;
-cp ./com.apple.dock.plist /Users/$1/Library/Preferences/com.apple.dock.plist;
-killall Dock
+# defaults write com.apple.dock persistent-apps -array;
+# cp ./com.apple.dock.plist /Users/$1/Library/Preferences/com.apple.dock.plist;
+# killall Dock
 
 # My default project directory
-cd ~/;
-mkdir -p workspace;
+# cd ~/;
+# mkdir -p workspace;
 
 # Configure Git
-git config --global init.defaultBranch main
-git config --global user.email "$2"
-git config --global user.name "$3"
+# git config --global init.defaultBranch main
+# git config --global user.email "$2"
+# git config --global user.name "$3"
+
+# Generate SSH Key
+ssh-keygen -t rsa
+pbcopy < ~/.ssh/id_rsa.pub
+echo "ssh public key copied to clip board. Add it to github profile at https://github.com/settings/profile"

fc63e734e3890bf62c7ab93fb164a62484cea385
Tyler Freeman <8192460+chiller-whale@users.noreply.github.com>
2021-04-21 18:19:43 -0400

    Adds echo statements to shell script

--- setup.sh
+++ setup.sh
@@ -1,27 +1,37 @@
 # #!/bin/bash
 
+echo "Installing oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+
 # # Install XCode developer tools
-# xcode-select --install;
+echo "Installing XCode Dev Tools..."
+xcode-select --install;
 
 # # Install brew and install apps / tools
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
-# brew bundle;
+echo "Installing Brew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+echo "Installing Applications"
+brew bundle;
 
 # Organize Dock
-# defaults write com.apple.dock persistent-apps -array;
-# cp ./com.apple.dock.plist /Users/$1/Library/Preferences/com.apple.dock.plist;
-# killall Dock
+echo "Organizing Dock..."
+defaults write com.apple.dock persistent-apps -array;
+cp ./com.apple.dock.plist /Users/$1/Library/Preferences/com.apple.dock.plist;
+killall Dock
 
 # My default project directory
+echo "Making directories..."
 # cd ~/;
 # mkdir -p workspace;
 
 # Configure Git
-# git config --global init.defaultBranch main
-# git config --global user.email "$2"
-# git config --global user.name "$3"
+echo "Configuring git..."
+git config --global init.defaultBranch main
+git config --global user.email "$2"
+git config --global user.name "$3"
 
 # Generate SSH Key
+echo "Generating RSA Key..."
 ssh-keygen -t rsa
 pbcopy < ~/.ssh/id_rsa.pub
 echo "ssh public key copied to clip board. Add it to github profile at https://github.com/settings/profile"

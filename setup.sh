c4c9e4872abd9862c9c031c1f9c4d6e0f2b2a008
Tyler Freeman <tylerfrmn5@gmail.com>
2021-04-21 20:21:08 -0400

    Updates gitignore and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,48 +1,40 @@
 # #!/bin/bash
-read -p "Login to 1Password Mac App. Press any key to continue. \n https://my.1password.com/signin?l=en"
-
 
 # My default project directory
 echo "Making directories..."
-cd ~/;
-mkdir -p workspace;
+mkdir -p ~/workspace;
 
 echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 
 # # Install XCode developer tools
-echo "Installing XCode Dev Tools..."
+echo "Installing XCode Dev Tools...";
 xcode-select --install;
 
 # # Install brew and install apps / tools
-echo "Installing Brew..."
+echo "Installing Brew...";
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
-echo "Installing Applications"
+echo "Installing Applications";
 brew bundle;
 
 # Going to need 1Password to login to stuff
-read -p "Login to 1Password - https://my.1password.com/signin?l=en. Press any key to continue."
+read -p "Login to 1Password - https://my.1password.com/signin?l=en. Press any key to continue.";
 
 # Organize Dock
-echo "Organizing Dock..."
+echo "Organizing Dock...";
 defaults write com.apple.dock persistent-apps -array;
 cp ./com.apple.dock.plist /Users/$1/Library/Preferences/com.apple.dock.plist;
-killall Dock
+killall Dock;
 
 # Configure Git
-echo "Configuring git..."
-git config --global init.defaultBranch main
-git config --global user.email "$2"
-git config --global user.name "$3"
-gh auth login
+echo "Configuring git...";
+git config --global init.defaultBranch main;
+git config --global user.email "$2";
+git config --global user.name "$3";
+gh auth login;
 
-# Generate SSH Key
-echo "Add key to github..."
-ssh-keygen -t rsa
-gh ssh-key add ~/.ssh/id_rsa.pub
-
-echo "Installing powerline fonts..."
-cd ~/workspace
-git clone https://github.com/powerline/fonts.git
-./fonts/install.sh
+echo "Installing powerline fonts...";
+cd ~/workspace;
+git clone https://github.com/powerline/fonts.git;
+./fonts/install.sh;
 
